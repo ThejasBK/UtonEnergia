af666e0b63322f92a99ffba3e47553fa409f854e
ThejasBK <thejaskiran99@gmail.com>
2020-09-17 11:45:10 +0530

    Registration and invite done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'register.dart';
+import 'signin.dart';
 //Splash screen changed in android/app/src/main/res/drawable -  android
 //Splash screen changed in ios/Runner/Assets.xcassets/launchImage.imageset - iOS
 void main() {
@@ -11,79 +12,66 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  Container input(String inp, bool obscurity) {
-    return Container(
-        margin: EdgeInsets.all(10),
-        //width: MediaQuery.of(context).size.width * 0.8,
-        child: new Theme(data: new ThemeData(
-            primaryColor: Colors.white30,
-            primaryColorDark: Colors.white
-        ), child: new TextField(
-          style: TextStyle(
-              color: Colors.white
-          ),
-          obscureText: obscurity,
-          cursorColor: Colors.white30,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white30)
-            ),
-            labelText: inp,
-            labelStyle: TextStyle(
-                color: Colors.white30
-            ),
-          ),
-        ))
-    );
-  }
-  Padding inputLabel(String name) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0),
-      child: Text(
-        name,
-        style: TextStyle(
-            color: Colors.white,
-            fontSize: 24
+  Builder userStatus(String status) {
+    return Builder(
+      builder: (context) => FlatButton(
+        onPressed: () {
+          if(status == 'REGISTER')
+            Navigator.push(context,
+                MaterialPageRoute(
+                    builder: (context) => Register()
+                )
+            );
+          else
+            Navigator.push(context,
+                MaterialPageRoute(
+                    builder: (context) => SignIn()
+                )
+            );
+        },
+        child: Card(
+            color: Colors.black,
+            child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ListTile(
+                  title: Center(child: Text(
+                      status,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24
+                      ),
+                  )),
+                )
+            )
         ),
       ),
     );
   }
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          backgroundColor: Colors.black,
-          body: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                inputLabel('USERNAME'),
-                input('USERNAME', false),
-                inputLabel("PASSWORD"),
-                input('PASSWORD', true),
-                Container(
-                  width: MediaQuery.of(context).size.width * 1,
-                  child: RaisedButton(
-                    onPressed: () {
-                      print('Hello');
-                    },
-                    color: Colors.black,
-                    child: Text(
-                      'SUBMIT',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24
-                      ),
-                    ),
-                    splashColor: Colors.white30,
-                  ),
-                )
-              ],
+      return MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              title: Text(
+                'fortyfive',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white
+                ),
+              ),
             ),
-          ),
-      ),
-    );
+            backgroundColor: Colors.black,
+            body: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  userStatus('REGISTER'),
+                  userStatus('SIGN IN')
+                ],
+              )
+            )
+          )
+      );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -1,7 +1,92 @@
 import 'package:flutter/material.dart';
 
-class Register {
-  void press() {
-    print("Pressed");
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  Container input(String inp, bool obscurity) {
+    return Container(
+        margin: EdgeInsets.all(10),
+        //width: MediaQuery.of(context).size.width * 0.8,
+        child: new Theme(data: new ThemeData(
+            primaryColor: Colors.white30,
+            primaryColorDark: Colors.white
+        ), child: new TextField(
+          style: TextStyle(
+              color: Colors.white
+          ),
+          obscureText: obscurity,
+          cursorColor: Colors.white30,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white30)
+            ),
+            labelText: inp,
+            labelStyle: TextStyle(
+                color: Colors.white30
+            ),
+          ),
+        ))
+    );
+  }
+
+  Padding inputLabel(String name) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0),
+      child: Text(
+        name,
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 24
+        ),
+      ),
+    );
+    @override
+    Widget build(BuildContext context) {
+      return Container();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                inputLabel('USERNAME'),
+                input('USERNAME', false),
+                inputLabel("PASSWORD"),
+                input('PASSWORD', true),
+                Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  child: RaisedButton(
+                    onPressed: () {
+                      print('Hello');
+                    },
+                    color: Colors.black,
+                    child: Text(
+                      'SUBMIT',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24
+                      ),
+                    ),
+                    splashColor: Colors.white30,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
