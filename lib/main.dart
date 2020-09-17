b7a71bdc3608f4e6411a83c460b278ed1288fefc
ThejasBK <thejaskiran99@gmail.com>
2020-09-17 11:17:41 +0530

    Register done

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final username = TextEditingController();
-  final password = TextEditingController();
-  final stadiumBorder = StadiumBorder(
-    side: BorderSide(color: Colors.red),
-  );
-  final roundedShape = RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(16),
-    side: BorderSide(color: Colors.red),
-  );
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -29,69 +20,87 @@ class MyApp extends StatelessWidget {
           body: Container(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                    'USERNAME',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24
-                    ),
-                ),
-                Container(
-                    margin: EdgeInsets.all(10),
-                    //width: MediaQuery.of(context).size.width * 0.8,
-                    child: TextField(
-                      obscureText: false,
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                      'USERNAME',
                       style: TextStyle(
-                        color: Colors.white
+                        color: Colors.white,
+                        fontSize: 24,
                       ),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'USERNAME',
-                        labelStyle: TextStyle(
-                          color: Colors.white30
-                        )
-                      ),
-                    )
-                ),
-                Text(
-                  'PASSWORD',
-                  style: TextStyle(
-                      color: Colors.white,
-                    fontSize: 24
                   ),
                 ),
                 Container(
                     margin: EdgeInsets.all(10),
                     //width: MediaQuery.of(context).size.width * 0.8,
-                    child: TextField(
+                    child: new Theme(data: new ThemeData(
+                        primaryColor: Colors.white30,
+                        primaryColorDark: Colors.white
+                    ), child: new TextField(
                       style: TextStyle(
                           color: Colors.white
                       ),
                       obscureText: true,
+                      cursorColor: Colors.white30,
                       decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'PASSWORD',
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white30)
+                          ),
+                          labelText: 'USERNAME',
                           labelStyle: TextStyle(
-                            color: Colors.white30
-                          )
+                              color: Colors.white30
+                          ),
+                      ),
+                    ))
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    'PASSWORD',
+                    style: TextStyle(
+                        color: Colors.white,
+                      fontSize: 24
                     ),
-                  )
+                  ),
                 ),
                 Container(
-                  margin: EdgeInsets.all(10),
-                  width: MediaQuery.of(context).size.width * 0.3,
+                    margin: EdgeInsets.all(10),
+                    //width: MediaQuery.of(context).size.width * 0.8,
+                    child: new Theme(data: new ThemeData(
+                      primaryColor: Colors.white30,
+                      primaryColorDark: Colors.white
+                    ), child: new TextField(
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                      obscureText: true,
+                      cursorColor: Colors.white30,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white30)
+                          ),
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              color: Colors.white30
+                          )
+                      ),
+                    ))
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 1,
                   child: RaisedButton(
                     onPressed: () {
                       print('Hello');
                     },
                     color: Colors.black,
                     child: Text(
-                        'SUBMIT',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24
-                        ),
+                      'SUBMIT',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24
+                      ),
                     ),
                     splashColor: Colors.white30,
                   ),
